bd23c83c145f1a35c445188f66675bba8a4bba65
ohmona <66560014+ohmona@users.noreply.github.com>
2023-05-31 17:13:15 +0200

    Add files via upload

--- lib/screens/quiz_screen.dart
+++ lib/screens/quiz_screen.dart
@@ -1,7 +1,10 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:vocabella/widgets/progress_bar_widget.dart';
 import 'package:vocabella/widgets/word_card_widget.dart';
+import 'package:vocabella/widgets/bottom_bar_widget.dart';
 
 class QuizScreen extends StatefulWidget {
   const QuizScreen({Key? key}) : super(key: key);
@@ -21,6 +24,28 @@ class _QuizScreenState extends State<QuizScreen> {
   // Variables about quiz
   bool wasWrong = false;
 
+  // Cards
+  late WordCard questionCard;
+  late WordCard answerCard;
+  late WordCard questionCard2;
+  late WordCard answerCard2;
+  late Stack cardStack;
+
+  // Bottom Bar
+  late BottomBox inputBox;
+  late BottomBox continueBox;
+
+  // Logics
+  late bool isOddTHCard;
+  late bool isShowingAnswer;
+
+  // Timer
+  late Timer transitionTimer;
+  late Timer disposalTimer;
+  late Timer showingTimer;
+  final int timerScale = 300;
+
+  // Runs when user gives enter
   void onSummit(String text) {
     if (text == answer) {
       wasWrong = false;
@@ -29,21 +54,248 @@ class _QuizScreenState extends State<QuizScreen> {
       fieldText.clear();
       setState(() {});
     }
+    questionCard.animSmall;
+    answerCard.animMedium;
   }
 
+  // Runs when user presses summit button
   void onSummitByButton() {
-    if (inputValue == answer) {
-      wasWrong = false;
+    onSummit(inputValue);
+  }
+
+  // Update stored data for input TextBox
+  void updateInputValue(String newInputValue) {
+    inputValue = newInputValue;
+  }
+
+  /*
+  *   Transition : Appearance of cards from bottom to top
+   */
+  // Timer variables
+  bool isTransitionRunning = false;
+  int transitionTime = 0;
+
+  // Body of transition timer
+  void onTransitionTick(Timer transitionTimer) {
+    if (transitionTime == timerScale) {
+      setState(() {
+        // answer card should be placed here
+        isOddTHCard ? answerCard.resetCenter() : answerCard2.resetCenter();
+        transitionTimer.cancel();
+        isTransitionRunning = false;
+        transitionTime = 0;
+      });
     } else {
-      wasWrong = true;
-      fieldText.clear();
-      setState(() {});
+      setState(() {
+        transitionTime++;
+      });
     }
+  }
+
+  // Trigger of transition Timer
+  void onTransitionStarted() {
+    transitionTimer = Timer.periodic(
+      const Duration(milliseconds: 1),
+      onTransitionTick,
+    );
+    setState(() {
+      isTransitionRunning = true;
+    });
+  }
+
+  /*
+  *   Disposal : Resetting previous cards into initial state
+   */
+  // Timer variables
+  bool isDisposalRunning = false;
+  int disposalTime = 0;
+
+  // Body of disposal timer
+  void onDisposalTick(Timer disposalTimer) {
+    if (disposalTime == timerScale) {
+      setState(() {
+        // answer card should be placed here
+        isOddTHCard ? answerCard2.reset() : answerCard.reset();
+        isOddTHCard ? questionCard2.reset() : questionCard.reset();
+        disposalTimer.cancel();
+        isDisposalRunning = false;
+        disposalTime = 0;
+      });
+    } else {
+      setState(() {
+        disposalTime++;
+      });
+    }
+  }
+
+  // Trigger of transition Timer
+  void onDisposalStarted() {
+    disposalTimer = Timer.periodic(
+      const Duration(milliseconds: 1),
+      onDisposalTick,
+    );
+    setState(() {
+      isDisposalRunning = true;
+    });
+  }
+
+  /*
+  *   Showing : Delay to answer showed
+   */
+  // Timer variables
+  bool isShowingRunning = false;
+  int showingTime = 0;
+
+  // Body of showing timer
+  void onShowingTick(Timer showingTimer) {
+    if (showingTime == timerScale) {
+      setState(() {
+        showingTimer.cancel();
+        isShowingRunning = false;
+        showingTime = 0;
+      });
+    } else {
+      setState(() {
+        showingTime++;
+      });
+    }
+  }
+
+  // Trigger of showing timer
+  void onShowingStarted() {
+    showingTimer = Timer.periodic(
+      const Duration(milliseconds: 1),
+      onShowingTick,
+    );
+    setState(() {
+      isShowingRunning = true;
+    });
+  }
+
+  // Show correct answer by animating cards sidewards
+  void showAnswer() {
+    if(isShowingRunning) {
+      return;
+    }
+    else if(isShowingAnswer) {
+      return;
+    }
+    else if(isTransitionRunning) {
+      return;
+    }
+
+    onShowingStarted();
+    isShowingAnswer = true;
+    if(isOddTHCard) {
+      questionCard.animSmall();
+      answerCard.animMedium();
+    }
+    else {
+      questionCard2.animSmall();
+      answerCard2.animMedium();
+    }
+  }
+
+  /* Show next cards by disposing (making invisible)
+  *  previous cards and making next card appeared */
+  void showNext() {
+    if(isTransitionRunning) {
+      return;
+    } else if(isDisposalRunning) {
+      return;
+    } else if(isShowingRunning) {
+      return;
+    } else if(!isShowingAnswer) {
+      return;
+    }
+
+    onDisposalStarted();
+    if(isOddTHCard) {
+      questionCard.animDisappear();
+      answerCard.animDisappear();
+      questionCard2.animAppear();
+      onTransitionStarted();
+    }
+    else {
+      questionCard2.animDisappear();
+      answerCard2.animDisappear();
+      questionCard.animAppear();
+      onTransitionStarted();
+    }
+    isOddTHCard = !isOddTHCard;
+    isShowingAnswer = false;
+    setState(() {});
   }
 
   @override
   void initState() {
     super.initState();
+
+    // Initialise 4 cards for test
+    questionCard = WordCard(
+      isQuestion: true,
+      isOddTHCard: true,
+      word: "data",
+      example: "Data fechted! Let's go",
+    );
+
+    answerCard = WordCard(
+      isQuestion: false,
+      isOddTHCard: true,
+      word: "Datei",
+      example: "Für Datenschutz muss man Datei schützen",
+    );
+
+    questionCard2 = WordCard(
+      isQuestion: true,
+      isOddTHCard: false,
+      word: "apple",
+      example: "",
+    );
+
+    answerCard2 = WordCard(
+      isQuestion: false,
+      isOddTHCard: false,
+      word: "Apfel",
+      example: "",
+    );
+
+    // Initialise InputTextBox
+    inputBox = BottomBox(
+      child: InputBox(
+        answer: answer,
+        inputValue: inputValue,
+        fieldText: fieldText,
+        onSummit: onSummit,
+        onSummitByButton: onSummitByButton,
+        updateInputValue: updateInputValue,
+      ),
+    );
+
+    // Initialise ContinueBox
+    continueBox = BottomBox(
+      child: ContinueBox(),
+    );
+
+    // Initialise Stack Component
+    cardStack = Stack(
+      clipBehavior: Clip.hardEdge,
+      children: [
+        answerCard,
+        questionCard,
+        answerCard2,
+        questionCard2,
+      ],
+    );
+
+    // Initialise some logic states and run first animation
+    isOddTHCard = true;
+    isShowingAnswer = false;
+    onTransitionStarted();
+  }
+
+  void removeContinueBox() {
+    //TODO solve issue that Bottombox doesn't disappear once function is called
   }
 
   @override
@@ -60,72 +312,23 @@ class _QuizScreenState extends State<QuizScreen> {
       ),
       body: Column(
         children: [
-          ProgressBar(),
+          const ProgressBar(), // TODO
           const SizedBox(
             height: 20,
           ),
-          Flexible(
-            flex: 1,
-            child: WordCard(),
+          Expanded(
+            child: cardStack,
           ),
           const SizedBox(height: 20),
-          Container(
-            decoration: BoxDecoration(
-              /*borderRadius: const BorderRadius.only(
-                topLeft: Radius.circular(10),
-                topRight: Radius.circular(10),
-              ),*/
-              color: Colors.grey,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey.withOpacity(0.5),
-                  blurRadius: 10,
-                  blurStyle: BlurStyle.normal,
-                ),
-              ],
-            ),
-            height: MediaQuery.of(context).size.height * 0.09,
-            child: Padding(
-              padding:
-                  EdgeInsets.all(MediaQuery.of(context).size.height * 0.01),
-              child: Container(
-                color: Colors.white,
-                child: Transform.translate(
-                  offset: const Offset(0, 6),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      border: InputBorder.none,
-                      floatingLabelAlignment: FloatingLabelAlignment.start,
-                      hintText: wasWrong ? answer : "",
-                      suffixIcon: IconButton(
-                        icon: Icon(Icons.send_rounded),
-                        color: Colors.grey.withOpacity(0.5),
-                        onPressed: onSummitByButton,
-                      ),
-                      prefixIcon: Icon(
-                        Icons.keyboard_alt_outlined,
-                        color: Colors.grey.withOpacity(0.5),
-                      ),
-                    ),
-                    style: const TextStyle(
-                      fontSize: 25,
-                      fontWeight: FontWeight.w400,
-                    ),
-                    controller: fieldText,
-                    cursorColor: Colors.grey.withOpacity(0.5),
-                    textAlign: TextAlign.center,
-                    onChanged: (text) {
-                      inputValue = text;
-                    },
-                    onSubmitted: onSummit,
-                  ),
-                ),
-              ),
-            ),
+          Stack(
+            children: [
+              inputBox,
+              continueBox,
+              FloatingActionButton(onPressed: isShowingAnswer? showNext : showAnswer, child: Icon(Icons.add_circle)),
+            ],
           ),
         ],
       ),
     );
   }
 }
-
--- lib/widgets/word_card_widget.dart
+++ lib/widgets/word_card_widget.dart
@@ -1,62 +1,265 @@
 import 'package:flutter/material.dart';
 
-class WordCard extends StatelessWidget {
-  const WordCard({Key? key}) : super(key: key);
+class WordCard extends StatefulWidget {
+  WordCard({Key? key, required this.word, required this.example, required this.isQuestion, required this.isOddTHCard,})
+      : super(key: key);
+
+  final String word, example;
+  final bool isQuestion, isOddTHCard;
+
+  late void Function() animAppear;
+  late void Function() animDisappear;
+  late void Function() animSmall;
+  late void Function() animMedium;
+  late void Function() reset;
+  late void Function() resetCenter;
+
+  @override
+  State<WordCard> createState() => _WordCardState();
+}
+
+class _WordCardState extends State<WordCard>
+    with TickerProviderStateMixin {
+
+  late Offset transformOffset;
+  late double scaleFactor;
+  late List<double> margins; // left, right, bottom, top
+  late double opacity;
+
+  // Some variables being responsible for animations
+  late AnimationController controller;
+  late Animation<double> animation;
+  late Animation<double> animation1;
+  late Animation<double> animation2;
+  late Animation<double> animation3;
+  late Animation<double> animation4;
+
+  // Values for animation
+  final int showingAnimDuration = 1000; // maybe configurable
+  final int transitionalAnimDuration = 1000; // maybe configurable
+  final Curve curveType = Curves.easeOutExpo; // maybe configurable
+
+  @override
+  void initState() {
+    super.initState();
+
+    margins = [10, 10, 0, 0];
+    transformOffset = const Offset(1000, 0);
+    scaleFactor = 1;
+    opacity = 1;
+
+    widget.animDisappear = animDisappear;
+    widget.animAppear = animAppear;
+    widget.animSmall = animSmall;
+    widget.animMedium = animMedium;
+    widget.reset = reset;
+    widget.resetCenter = resetCenter;
+
+    if(widget.isQuestion && widget.isOddTHCard) animAppear();
+  }
+
+  // Animation once called
+  void animAppear() {
+    if(!widget.isQuestion) return;
+
+    controller = AnimationController(
+        vsync: this, duration: Duration(milliseconds: transitionalAnimDuration));
+    final Animation<double> curve =
+        CurvedAnimation(parent: controller, curve: curveType);
+    animation = Tween<double>(begin: 1000, end: 0).animate(curve);
+
+    animation.addListener(() {
+      transformOffset = Offset(0, animation.value);
+      setState(() {});
+    });
+    controller.forward();
+  }
+
+  // Animation for question-card
+  void animSmall() {
+    controller = AnimationController(
+        vsync: this, duration: Duration(milliseconds: showingAnimDuration));
+    final Animation<double> curve =
+        CurvedAnimation(parent: controller, curve: curveType);
+    animation = Tween<double>(begin: 10, end: 100).animate(curve); // scale side
+    animation1 = Tween<double>(begin: 10, end: 170).animate(curve); // scale up, down
+    animation2 = Tween<double>(begin: 1, end: 0.7).animate(curve); // scale
+    animation3 = Tween<double>(begin: 0, end: -120).animate(curve); // x
+    animation4 = Tween<double>(begin: 0, end: -200).animate(curve); // y
+
+    animation.addListener(() {
+      double left = animation.value;
+      double right = animation.value;
+      double bottom = animation1.value;
+      double top = animation1.value;
+      margins = [left, right, bottom, top];
+
+      scaleFactor = animation2.value;
+
+      transformOffset = Offset(animation3.value, animation4.value);
+      setState(() {});
+    });
+    controller.forward();
+  }
+
+  // Animation for answer-card
+  void animMedium() {
+    controller = AnimationController(
+        vsync: this, duration: Duration(milliseconds: showingAnimDuration));
+    final Animation<double> curve =
+        CurvedAnimation(parent: controller, curve: curveType);
+    animation = Tween<double>(begin: 10, end: 25).animate(curve); // scale side
+    animation1 =
+        Tween<double>(begin: 10, end: 50).animate(curve); // scale up,down
+    animation2 = Tween<double>(begin: 1, end: 0.8).animate(curve); // scale
+    animation3 = Tween<double>(begin: 0, end: 50).animate(curve); // x
+    animation4 = Tween<double>(begin: 0, end: 90).animate(curve); // y
+
+    animation.addListener(() {
+      double left = animation.value;
+      double right = animation.value;
+      double bottom = animation1.value;
+      double top = animation1.value;
+      margins = [left, right, bottom, top];
+
+      scaleFactor = animation2.value;
+
+      transformOffset = Offset(animation3.value, animation4.value);
+
+      setState(() {});
+    });
+    controller.forward();
+  }
+
+  // Animation for disposal
+  void animDisappear() {
+    controller = AnimationController(
+        vsync: this, duration: Duration(milliseconds: transitionalAnimDuration));
+    final Animation<double> curve =
+    CurvedAnimation(parent: controller, curve: curveType);
+
+    // Animation setup
+    animation = Tween<double>(begin: 1, end: 0).animate(curve);
+
+    animation.addListener(() {
+      opacity = animation.value;
+      setState(() {});
+    });
+    controller.forward();
+  }
+
+  // Set state into default value, so that it places under main widget
+  void reset() {
+    setState(() {
+      margins = [10, 10, 0, 0];
+      transformOffset = const Offset(1000, 0);
+      scaleFactor = 1;
+      opacity = 1;
+    });
+  }
+
+  // Set state into default value but locate it at the middle
+  void resetCenter() {
+    setState(() {
+      margins = [10, 10, 0, 0];
+      transformOffset = const Offset(0, 0);
+      scaleFactor = 1;
+      opacity = 1;
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(left: 10, right: 10),
-      padding: const EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(30),
-        color: Colors.white,
-        border: Border.all(
-          color: Theme.of(context).cardColor,
-          width: 10,
+    return Transform.translate(
+      offset: transformOffset,
+      child: Transform.scale(
+        scale: scaleFactor,
+        child: Opacity(
+          opacity: opacity,
+          child: Container(
+            margin: EdgeInsets.only(
+                left: margins[0],
+                right: margins[1],
+                bottom: margins[2],
+                top: margins[3]),
+            padding: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              color: Colors.white,
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  Colors.grey.withOpacity(0.11),
+                  Colors.white.withOpacity(0.1),
+                ],
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(1),
+                  offset: const Offset(4, 4),
+                  blurRadius: 15,
+                  spreadRadius: 1,
+                  blurStyle: BlurStyle.normal,
+                ),
+                BoxShadow(
+                  color: Colors.white.withOpacity(1),
+                  offset: const Offset(-4, -4),
+                  blurRadius: 15,
+                  spreadRadius: 1,
+                  blurStyle: BlurStyle.normal,
+                ),
+              ],
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  widget.word,
+                  style: const TextStyle(
+                    fontSize: 50,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                const SizedBox(height: 30),
+                Text(
+                  widget.example,
+                  style: const TextStyle(
+                    fontSize: 16,
+                    color: Colors.grey,
+                  ),
+                ),
+                SizedBox(height: MediaQuery.of(context).size.height * 0.05),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    IconButton(
+                      onPressed: animDisappear,
+                      icon: const Icon(
+                        Icons.audiotrack_rounded,
+                        size: 35,
+                      ),
+                    ),
+                    const SizedBox(width: 30),
+                    IconButton(
+                      onPressed: widget.isQuestion ? animSmall : animMedium,
+                      icon: const Icon(
+                        Icons.audiotrack_rounded,
+                        size: 35,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
         ),
-        boxShadow: [
-          BoxShadow(
-            color: Theme.of(context).cardColor.withOpacity(0.5),
-            blurRadius: 10,
-            blurStyle: BlurStyle.normal,
-          ),
-        ],
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            "data",
-            style: TextStyle(
-              fontSize: 50,
-              fontWeight: FontWeight.w400,
-            ),
-          ),
-          const SizedBox(height: 30),
-          Text(
-            "data fechted! Let's go",
-            style: TextStyle(
-              fontSize: 16,
-              color: Colors.grey,
-            ),
-          ),
-          SizedBox(height: MediaQuery.of(context).size.height * 0.05),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Icon(
-                Icons.audiotrack_rounded,
-                size: 35,
-              ),
-              const SizedBox(width: 30),
-              const Icon(
-                Icons.audiotrack_rounded,
-                size: 35,
-              ),
-            ],
-          ),
-        ],
       ),
     );
   }
